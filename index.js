48c7f95280f58d61ea9748737d5cfba21d63a503
hannan-abdul <shohas563@gmail.com>
2021-04-18 14:23:25 +0600

    changes

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get('/', (req, res) => {
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4ko6w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-// console.log(process.env.DB_NAME)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const serviceCollection = client.db("homeRenovation").collection("renovationService");
@@ -60,14 +59,10 @@ client.connect(err => {
     console.log(req.params.id);
     serviceCollection.deleteOne({_id: ObjectID(req.params.id) })
       .then(result => {
-        // console.log(result)
         res.send(result.deletedCount > 0);
       })
   })
 
 });
-
-
-
 
 app.listen(process.env.PORT || port)
